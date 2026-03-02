e3d24a193505141fd0e83df2da93a0f27c3ab6e1
Rafael Mendonça <rafambn@gmail.com>
2026-03-01 21:46:45 -0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,36 @@
+rootProject.name = "KMP-VPN"
+
+pluginManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+include(":kmp-vpn")
+include(":sample:composeApp")
+
